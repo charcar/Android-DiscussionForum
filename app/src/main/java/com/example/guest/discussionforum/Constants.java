2234030902f7a1475e0d6327d58c95764b1ed319
Brad Pizzimenti <brad@gennakersystems.com>
2016-05-03 11:51:17 -0700

    fix write new thread to database

--- app/src/main/java/com/example/guest/discussionforum/Constants.java
+++ app/src/main/java/com/example/guest/discussionforum/Constants.java
@@ -5,5 +5,5 @@ package com.example.guest.discussionforum;
  */
 public class Constants {
     public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
-
+    public static final String FIREBASE_THREADS = FIREBASE_URL + "/threads";
 }
--- app/src/main/java/com/example/guest/discussionforum/ui/NewDiscussionActivity.java
+++ app/src/main/java/com/example/guest/discussionforum/ui/NewDiscussionActivity.java
@@ -2,14 +2,45 @@ package com.example.guest.discussionforum.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.guest.discussionforum.Constants;
 import com.example.guest.discussionforum.R;
+import com.example.guest.discussionforum.models.Discussion;
+import com.firebase.client.Firebase;
 
-public class NewDiscussionActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class NewDiscussionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.subjectEditText) EditText mSubjectEditText;
+    @Bind(R.id.bodyEditText) EditText mBodyEditText;
+    @Bind(R.id.submitNewTopicButton) Button mSubmitNewTopicButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_discussion);
+        ButterKnife.bind(this);
+
+        mSubmitNewTopicButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String subject = mSubjectEditText.getText().toString();
+        String body = mBodyEditText.getText().toString();
+        Discussion newThread = new Discussion(subject, body);
+        saveThreadToFirebase(newThread);
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void saveThreadToFirebase(Discussion newThread) {
+        Firebase newThreadRef = new Firebase(Constants.FIREBASE_THREADS);
+        newThreadRef.push().setValue(newThread);
     }
 }
