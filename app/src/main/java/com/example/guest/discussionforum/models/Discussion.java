ef6f0acbf3b0e5e9eafc11ffce047baaf6bebf67
Brad Pizzimenti <brad@gennakersystems.com>
2016-05-03 13:42:50 -0700

    add date to thread model

--- app/src/main/java/com/example/guest/discussionforum/models/Discussion.java
+++ app/src/main/java/com/example/guest/discussionforum/models/Discussion.java
@@ -1,5 +1,7 @@
 package com.example.guest.discussionforum.models;
 
+import java.util.Date;
+
 /**
  * Created by Guest on 5/3/16.
  */
@@ -7,12 +9,20 @@ package com.example.guest.discussionforum.models;
 public class Discussion {
     String subject;
     String body;
+    Date date;
 
     public Discussion(){}
+
 
     public Discussion(String subject, String body) {
         this.subject = subject;
         this.body = body;
+        this.date = new Date();
+    }
+
+
+    public Date getDate() {
+        return date;
     }
 
     public String getSubject() {
